5310a5e706c7b8923dd4b9f1415fc0a2b2b978e1
marcos-at-968684553621 <lpbalves7filho@gmail.com>
2024-07-23 18:18:13 -0300

    hubbi test incompleted

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,25 @@
-// src/index.ts
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 
 const app = express();
 
-app.use(cors());
+// Configuração do CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use("/auth", authRoutes);
 
